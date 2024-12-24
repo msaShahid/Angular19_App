0f525608cfe148d562720893f60a9a9372e0dccb
Mohammad Shahid <msa.ansari28@gmail.com>
2024-12-24 22:26:16 +0530

    `Added form fields and functionality to reactive form component`

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
-
+  useForm: FormGroup = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+    age: new FormControl(""),
+    country: new FormControl(""),
+    state: new FormControl("Goa"),
+    city: new FormControl(""),
+    zipcode: new FormControl(""),
+    isTermsAggree:  new FormControl(false)
+  })
 }
