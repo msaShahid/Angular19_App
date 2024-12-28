a2a383282f3dbaf315af3c85e8e3a99afbb26c6d
Mohammad Shahid <msa.ansari28@gmail.com>
2024-12-28 23:09:03 +0530

    `Removed unused import statements from AppComponent`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { AdminComponent } from './components/admin/admin.component';
-import { UserComponent } from './components/user/user.component';
-import { DataBindingComponent } from "./components/data-binding/data-binding.component";
-import { NgIfComponent } from "./components/ng-if/ng-if.component";
-import { NgForComponent } from "./components/ng-for/ng-for.component";
-import { NgClassComponent } from './components/ng-class/ng-class.component';
-import { NgStyleComponent } from './components/ng-style/ng-style.component';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
