7a587306642c0cbfeae9d328700a616718ec7aff
Mohammad Shahid <msa.ansari28@gmail.com>
2024-12-28 23:07:28 +0530

    `Added ngx-spinner to display loading animation and updated dependencies in package-lock.json and package.json`

--- src/app/components/API/get-api/get-api.component.ts
+++ src/app/components/API/get-api/get-api.component.ts
@@ -1,12 +1,13 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MasterService } from '../../../service/master.service';
 import { IUserDetails } from '../../../model/interface/master';
-import { catchError, throwError } from 'rxjs';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-get-api',
-  imports: [],
+  imports: [NgxSpinnerModule, CommonModule],
   templateUrl: './get-api.component.html',
   styleUrl: './get-api.component.css'
 })
@@ -14,19 +15,22 @@ export class GetApiComponent {
 
   userList: IUserDetails[] = [];
 
-  constructor(private masterService: MasterService){}
+  constructor(private masterService: MasterService, private spinner: NgxSpinnerService){}
 
   ngOnInit(): void {
     this.getUserDetails()
   }
 
   getUserDetails(){
+    this.spinner.show()
     this.masterService.getUser().subscribe({
       next: (response: IUserDetails[]) => {
         this.userList = response;
+        this.spinner.hide()
       },
       error: (error: any) => {
-        console.error(error); 
+        console.error('Error fetching data : ', error);
+        this.spinner.hide()
       }
     });
   }
