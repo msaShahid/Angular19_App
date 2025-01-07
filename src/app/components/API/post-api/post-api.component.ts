dbe5e0161d42d37c16520a1fcdbdb17d3a68282c
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-07 23:54:58 +0530

    `Added ngModel to form fields and implemented save functionality for new car`

--- src/app/components/API/post-api/post-api.component.ts
+++ src/app/components/API/post-api/post-api.component.ts
@@ -1,20 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-api',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-api.component.html',
   styleUrl: './post-api.component.css'
 })
 export class PostApiComponent {
 
   carList: any[] =[]
+  carObj: any = {
+    "CarId": 0,
+    "Brand": "",
+    "Model": "",
+    "Year": "",
+    "Color": "",
+    "DailyRate": "",
+    "CarImage": "",
+    "RegNo": ""
+  }
   http = inject(HttpClient);
 
   getAllCarList(){
     this.http.get("https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars").subscribe((res:any) =>{
       this.carList = res.data;
+    })
+  }
+
+  saveNewCar(){
+    this.http.post("https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewCar", this.carObj).subscribe((res:any) => {
+     // debugger;
+      if(res.result){
+        alert("Car Added Successfully");
+      }else{
+        alert(res.message);
+      }
     })
   }
 
