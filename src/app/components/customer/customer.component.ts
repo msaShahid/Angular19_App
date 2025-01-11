94c418e1bf674e77fca3211b356a1fe67812eb15
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-11 21:56:07 +0530

    Added customer management UI

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,11 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+
+  customerObject: any = {
+    "customerId": 0,
+    "customerName": "",
+    "customerMobile": "",
+    "customerEmail": "",
+    "customerCity": ""
+  };
+
+  customerDetails: any [] = [];
+
+  constructor(private http: HttpClient){
+    this.getCustomer();
+  }
+
+  getCustomer(){
+    this.http.get("https://freeapi.miniprojectideas.com/api/carRentalApp/GetCustomers").subscribe((res: any) => {
+      this.customerDetails = res.data;
+    })
+
+  }
 
 }
