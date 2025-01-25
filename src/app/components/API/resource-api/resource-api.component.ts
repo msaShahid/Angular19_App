f2be16073f2c30f96a31c3ddd925380da0d7e4ee
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-25 11:48:05 +0530

    `Update dropdown menu in app.component.html and add new route in app.routes.ts`

---
+++ src/app/components/API/resource-api/resource-api.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-api',
+  imports: [],
+  templateUrl: './resource-api.component.html',
+  styleUrl: './resource-api.component.css'
+})
+export class ResourceApiComponent {
+
+}
