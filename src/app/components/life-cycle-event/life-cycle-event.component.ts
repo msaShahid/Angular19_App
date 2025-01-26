9b900c959d9b793efc141482fdd69eda1eacf11f
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-26 12:08:27 +0530

    `Update LifeCycleEventComponent to implement multiple lifecycle hooks`

--- src/app/components/life-cycle-event/life-cycle-event.component.ts
+++ src/app/components/life-cycle-event/life-cycle-event.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-event',
@@ -6,6 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './life-cycle-event.component.html',
   styleUrl: './life-cycle-event.component.css'
 })
-export class LifeCycleEventComponent {
+export class LifeCycleEventComponent implements OnInit, AfterViewInit, AfterViewChecked, AfterContentInit, AfterContentChecked, OnDestroy {
+
+  constructor(){
+    console.log('constructor called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit.');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit.');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked.');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit.');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked.');
+  }
+
+  ngOnDestroy(): void {
+    alert("Are you sure to leave this page?")
+    console.log('ngOnDestory')
+  }
 
 }
