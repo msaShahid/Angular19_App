4e472e5c157c0e18536aac4bddc50cfe7d7be77a
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-27 22:41:31 +0530

    `Added container and date formatting, modified option text in select element by using pipes

--- src/app/components/ng-for/ng-for.component.ts
+++ src/app/components/ng-for/ng-for.component.ts
@@ -1,15 +1,16 @@
-import { NgFor } from '@angular/common';
+import {JsonPipe, NgFor, UpperCasePipe,LowerCasePipe, DatePipe,  } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [NgFor],
+  imports: [NgFor, UpperCasePipe, LowerCasePipe, JsonPipe, DatePipe],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
 export class NgForComponent {
 
   cityList: string[] = ["Pune","Nagpur","Jaipur","Mumbai","Thane"];
+  currentDate: Date = new Date();
 
   employeObject: any[] = [
     {"empid":101,"name": "Rahul", "age": 25, "city": "Pune"},
