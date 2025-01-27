e3a4de6dc12d6549b8e8d1cf9f731bcf97f53611
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-27 23:36:06 +0530

    `Added progress bar component and used it in ng-for component to display employee attendance`

--- src/app/components/common/progressbar/progrss-bar/progrss-bar.component.ts
+++ src/app/components/common/progressbar/progrss-bar/progrss-bar.component.ts
@@ -1,14 +1,15 @@
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progrss-bar',
-  imports: [NgStyle],
+  imports: [NgStyle, NgClass],
   templateUrl: './progrss-bar.component.html',
   styleUrl: './progrss-bar.component.css'
 })
 export class ProgrssBarComponent {
 
   @Input() progressValue: number = 0
+  @Input() bgColor: string = 'bg-success'; 
 
 }
--- src/app/components/ng-for/ng-for.component.ts
+++ src/app/components/ng-for/ng-for.component.ts
@@ -1,10 +1,11 @@
 import {JsonPipe, NgFor, UpperCasePipe,LowerCasePipe, DatePipe,  } from '@angular/common';
 import { Component } from '@angular/core';
 import { CustomPipesPipe } from '../pipes/custom-pipes.pipe';
+import { ProgrssBarComponent } from "../common/progressbar/progrss-bar/progrss-bar.component";
 
 @Component({
   selector: 'app-ng-for',
-  imports: [NgFor, UpperCasePipe, LowerCasePipe, JsonPipe, DatePipe, CustomPipesPipe],
+  imports: [NgFor, UpperCasePipe, LowerCasePipe, JsonPipe, DatePipe, CustomPipesPipe, ProgrssBarComponent],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
@@ -14,11 +15,22 @@ export class NgForComponent {
   currentDate: Date = new Date();
 
   employeObject: any[] = [
-    {"empid":101,"name": "Rahul", "age": 25, "city": "Pune"},
-    {"empid":102,"name": "", "age": 25},
-    {"empid":103,"name": "Alex", "age": 25, "city": "Thane"},
-    {"empid":104,"name": "Rohan", "age": 25, "city": "Mumbai"},
+    {empid:101,name: "Rahul", age: 25, city: "Pune", attendance: 40},
+    {empid:102,name: "", age: 25,attendance: 75},
+    {empid:103,name: "Alex", age: 25, city: "Thane",attendance: 72},
+    {empid:104,name: "Rohan", age: 25, city: "Mumbai",attendance: 100},
     
   ]
+
+  getAttendanceClass(attendance: number): string {
+    if (attendance < 50) {
+      return 'bg-danger'; 
+    } else if (attendance < 75) {
+      return 'bg-warning'; 
+    } else {
+      return 'bg-success'; 
+    }
+  }
+  
 
 }
