4870b3f4bd06716441049773284486df2db500fe
Mohammad Shahid <msa.ansari28@gmail.com>
2025-01-27 23:11:34 +0530

    `Added progress value to progress bar component and updated parent components to pass progress value as input.`

--- src/app/components/common/progressbar/progrss-bar/progrss-bar.component.ts
+++ src/app/components/common/progressbar/progrss-bar/progrss-bar.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progrss-bar',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './progrss-bar.component.html',
   styleUrl: './progrss-bar.component.css'
 })
 export class ProgrssBarComponent {
+
+  @Input() progressValue: number = 0
 
 }
