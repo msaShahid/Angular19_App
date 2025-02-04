fee2bc9c2bce080f2889fefa98b8abee2b0f257d
Mohammad Shahid <msa.ansari28@gmail.com>
2025-02-04 23:59:14 +0530

    `Added logout button and functionality to layout component`

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet, } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+
+  router = inject(Router)
+
+  onLogout(){
+    localStorage.removeItem('user');
+    this.router.navigateByUrl('login');
+  }
 
 }
