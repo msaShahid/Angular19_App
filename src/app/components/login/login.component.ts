9cb91bc1c15f8ebb90dac577c21b14eb1ff84645
Mohammad Shahid <msa.ansari28@gmail.com>
2025-02-04 23:33:04 +0530

    `feat(login): updated login form to use ngModel and added login functionality`

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginObj: any = {
+    username: '',
+    password: '',
+  }
+
+  onLogin(){
+    console.log(this.loginObj);
+    if(this.loginObj.username == 'admin' && this.loginObj.password == 'admin@123'){
+      console.log('Login Successfull');
+
+    }else{
+      alert('wrong credentials');
+    }
+  }
 
 }
