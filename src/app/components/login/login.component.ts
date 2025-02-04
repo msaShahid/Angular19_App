41e9c82b075525c125fdd28851e2249a67592f94
Mohammad Shahid <msa.ansari28@gmail.com>
2025-02-04 23:52:29 +0530

    `Refactor login form and API call in login component`

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -11,21 +12,36 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
 
   router = inject(Router);
+  http = inject(HttpClient)
 
   loginObj: any = {
     username: '',
     password: '',
   }
+  // adminsd@gmail.com & admin123
+  apiLoginObj: any = {
+    "EmailId": "",
+    "Password": ""
+  }
 
   onLogin(){
-    console.log(this.loginObj);
-    if(this.loginObj.username == 'admin' && this.loginObj.password == 'admin@123'){
-      console.log('Login Successfull');
-      this.router.navigateByUrl("admin");
+    console.log(this.apiLoginObj);
+    // if(this.loginObj.username == 'admin' && this.loginObj.password == 'admin@123'){
+    //   console.log('Login Successfull');
+    //   this.router.navigateByUrl("admin");
 
-    }else{
+    // }else{
+    //   alert('wrong credentials');
+    // }
+
+    this.http.post("https://projectapi.gerasim.in/api/UserApp/login", this.apiLoginObj).subscribe((res:any) => {
+    console.log(res);
+     localStorage.setItem("user",res.data.userId);
+     this.router.navigateByUrl("admin");
+    },
+    error => {
       alert('wrong credentials');
-    }
+    });
   }
 
 }
