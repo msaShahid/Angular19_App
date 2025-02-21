180c382a694dd38542658b2ac71537127171e636
Mohammad Shahid <msa.ansari28@gmail.com>
2025-02-21 23:09:53 +0530

    `Added conditional form fields for forgot password component`

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,16 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
 
+  isResetForm: boolean = false;
+
+  resetObj: any = {
+    "Email": "",
+    "Otp":  "",
+    "NewPassword": "",
+  }
 
   sentOtp(){
+    this.isResetForm = true;
     console.log('otp');
+  }
+
+  resetPassword(){
+    
   }
 
 }
